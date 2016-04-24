bab105efbc50af200b6f25b7cc834ba956cfa630
Richard Ahn <richahn2@gmail.com>
2016-04-24 16:43:21 -0400

    Added some more topic tests

--- test/topic-test.js
+++ test/topic-test.js
@@ -4,33 +4,53 @@ var mongoose = require('mongoose');
 var server = require('../server');
 var exceptions = require('../exceptions');
 
-var topicTitle = "A discussion ssaabout abortion";
-var topic = {
-    "title": topicTitle
-}
+var topicTitleOne = "A discussion ssaabout abortion";
+var topicTitleTwo = "Politics";
 
-describe('Tests the Topic api controller', function() {
-    it('POST for /api/topics', function(done) {
+var postTestGood = function(topicTitle) {
+    return function(done) {
+        var topic = {
+            "title": topicTitle
+        }
         request(server)
             .post('/api/topics')
             .type('form')
             .send(topic)
             .end(function(err, res) {
-                console.log(err);
-                console.log(res.body);
                 if (!err) {
                     assert.equal(res.status, 200);
                     assert.equal(res.body.message, 'Successfully added a topic');
                     done();
                 } else {
-                    assert.equal(res.status, exceptions.default.status);
-                    assert.equal(res.body, exceptions.default.error);
-                    done(err);
+                    done();
                 }
             });
-    });
+    }
+}
+var postTestDuplicate = function(topicTitle) {
+    return function(done) {
+        var topic = {
+            "title": topicTitle
+        }
 
-    it('DELETE for /api/topics/' + topicTitle, function(done) {
+        request(server)
+            .post('/api/topics')
+            .type('form')
+            .send(topic)
+            .end(function(err, res) {
+                if (err) {
+                    assert.equal(res.status, exceptions.default.status);
+                    assert.equal(res.body, exceptions.default.error);
+                    done();
+                } else {
+                    done();
+                }
+
+            });
+    };
+};
+var deleteTestGood = function(topicTitle) {
+    return function(done) {
         request(server)
             .delete('/api/topics/' + topicTitle)
             .end(function(err, res) {
@@ -44,5 +64,14 @@ describe('Tests the Topic api controller', function() {
                     done(err);
                 }
             });
-    });
+    };
+};
+
+
+describe('Tests the Topic api controller', function() {
+    it('POST 1 for /api/topics', postTestGood(topicTitleOne));
+    it('POST 2 for /api/topics', postTestGood(topicTitleTwo));
+    it('Duplicate POST for /api/topics', postTestDuplicate(topicTitleOne));
+    it('DELETE for /api/topics/' + topicTitleOne, deleteTestGood(topicTitleOne));
+    it('DELETE for /api/topics/' + topicTitleTwo, deleteTestGood(topicTitleTwo));
 });
